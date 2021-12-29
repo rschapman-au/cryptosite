a96b6c089bc017bc3f7638abe35487185369a26d
Ryan Chapman <rschapman.au@gmail.com>
2021-12-29 19:59:47 +1100

    added initial Crypto news page and linked to Bing

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Select, Typography, Row, Col, Avatar, Card} from 'antd';
+import moment from 'moment';
 
-const News = () => {
+import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+
+const { Text, Title } = Typography;
+const { Option } =  Select;
+
+
+
+const News = ({simplified}) => {
+    const [newsCategory, setNewsCategory] = useState('cryptocurrency');
+    const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+
+    
+    if (!cryptoNews?.value) return 'Data Loading...';
+
     return (
-        <div>
-            News
-        </div>
+        <Row gutter={[24,24]}>
+            {cryptoNews.value.map((news, i)=> (
+                <Col xs={24} sm={12} lg={8} key={i}>
+                    <Card hoverable className="news-card">
+                        <a href={news.url} target="_blank" rel="noreferrer">
+                            <div className="news-image-container">
+                                <Title className="news-title" level={4}>{news.name}</Title>
+                            </div>
+                        </a>
+                    </Card>
+                </Col>
+
+            ))}
+        </Row>
     )
 }
 
